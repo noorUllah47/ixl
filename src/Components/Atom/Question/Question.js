52cb3d9ea86117a5bd29e32a40e6190ea41fe2ee
unknown <noorullah@u2ventures.io>
2022-06-01 15:11:09 +0500

    complexity replace by param id

--- src/Components/Atom/Question/Question.js
+++ src/Components/Atom/Question/Question.js
@@ -27,7 +27,7 @@ const Question = (props) => {
     }, [props.param_id,inputval])
     useEffect(() => {
     
-        if(props.details?.UserComplexityLevel===1){
+        if(props.param_id==="1"){
             setModeltogle(graphvalues)}
             else{
            setModeltogle(inputval)
@@ -47,7 +47,7 @@ const Question = (props) => {
     //     }
         // else{
             
-            if(props.details?.UserComplexityLevel===1){
+            if(props.param_id==="1"){
                 props.chan(graphvalues)
             try{
            
@@ -113,7 +113,7 @@ const Question = (props) => {
 function handleNext(){
     window.reload()
 }
-  console.log("modeltogooooooooooooo",modeltogle)
+  console.log("modeltogooooooooooooo",modeltogle,props.param_id)
     return (
         <>
             <p>
@@ -143,11 +143,11 @@ function handleNext(){
             <div className="mt-3 d-flex ">
             {/* {modeltogle }---{props.ans} */}
                     <button type="submit" className="bg-lgtGreen border-0 text-white rounded px-3 py-1" data-bs-toggle={modeltogle?.replace(/\s/g, '') !== props.ans.replace(/\s/g, '')? "":"modal"} data-bs-target="#exampleModal" onClick={hidemodal}>Submit</button>
-                   {/* { props.param_id==="2"? <Link to={{ pathname:`/questions/${3}`,state: { name: "Ali" }}}className=' btn btn-primary skill_link mx-2' >Next </Link>
+                   {/* { props.param_id==="2"? <Link to={{ pathname:`/questions/${3}`,state: { name: "Ali" }}}className=' btn btn-primary skill_link mx-2'onClick={handleNext} >Next </Link>
 :""}
    { props.param_id==="3"? <Link to={{ pathname:`/questions/${2}`,state: { name: "Ali" }}} onClick={handleNext} className=' btn btn-primary skill_link mx-2' >back </Link>
-:""}
-                   */}
+:""} */}
+                  
 
             </div>
 
@@ -158,8 +158,8 @@ function handleNext(){
                             <section aria-live="assertive" aria-atomic="true" className="accolades">
                                 <img className="cae-checkmark" src="data:image/svg+xml,%3csvg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='67px' height='60px' viewBox='0 0 67 60' style='enable-background:new 0 0 67 60;' xml:space='preserve'%3e %3cstyle type='text/css'%3e .st0%7bfill-rule:evenodd;clip-rule:evenodd;fill:%238CC714;%7d %3c/style%3e %3cg%3e %3cpath class='st0' d='M5,27c0,0,5.1,2.5,9.5,5.7c2.7,2,6.8,4.9,6.8,4.9S30.4,26.6,41,17C51.4,7.6,62,0,62,0l5,5c0,0-13.6,13.8-25,28 C31.4,46.2,23,59.9,23,60c0,0-11.7-16.2-23-28L5,27z'/%3e %3c/g%3e %3c/svg%3e" alt="" />
                                 <h4>Fantastic!</h4></section>
-                        </div>
-                        <button type="button" id="cls" className="close border-0" data-bs-dismiss="modal" aria-label="Close">
+                             </div>
+                            <button type="button" id="cls" className="close border-0" data-bs-dismiss="modal" aria-label="Close">
         
                             {/* <span aria-hidden="true">&times;</span> */}
                         </button>
